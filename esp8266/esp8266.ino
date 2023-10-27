6015b1612419b2e39c314d775aa83e39d4ea36a4
Murphy <631001833@qq.com>
2023-10-28 00:55:52 +0800

    增加用串口发送数据 可以上传到数据库 ，修改功能

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -13,6 +13,7 @@ const char* server = "fc-mp-f86e4895-a6ec-4a1c-a14f-141f3d09c694.next.bspapp.com
 int port = 443;
 int step=0;
 int connectStep=0;
+
 String  ssid;
 String  password;
 Ticker timer;
@@ -72,79 +73,120 @@ void loop() {
     switch(step)
     {
 
-    case 0: 
-    {
-        if (Serial.available()) { // 检查串口缓冲区是否有可用数据
-            String data = Serial.readStringUntil('\n'); // 读取一行数据,,数据要加换行符才可以
-            if (data == "postConnect") {
-              step = 1;
-            }else if (data == "ConnectWifi")
-            {
-              connectStep = 0;
-              step = 2;
-            }
-            else if (data == "GetWifi")
-            {
-              step = 3;
-            }
-          
-        }
-
-    }break;
-    case 1: 
-    {
-        postConnect();
-        step = 0;
-    }break;
-    case 2: 
-    {
-
-      switch(connectStep)
+      case 0: 
       {
-        case 0:
-        {
-            connectStep = 1;
-            Serial.println(" please input ssid");
-        }break;
-        case 1:
-        {
-          if (Serial.available()) { 
-            ssid = Serial.readStringUntil('\n'); // 读取一行数据,数据要加换行符才可以
-            ssid.trim();
-            Serial.printf("you input ssid is %s and please input password\n ", ssid);
-            connectStep = 2;
-          }
-        }break;
-        case 2:
-        {
-          if (Serial.available()) {
-            password = Serial.readStringUntil('\n'); // 读取一行数据,,数据要加换行符才可以
-            password.trim();
-             Serial.printf("you input password is %s\n", password);
-            connectStep = 3;
+          if (Serial.available()) { // 检查串口缓冲区是否有可用数据
+              String data = Serial.readStringUntil('\n'); // 读取一行数据,,数据要加换行符才可以
+              if (data == "postConnect") {
+                step = 1;
+              }else if (data == "ConnectWifi")
+              {
+                connectStep = 0;
+                step = 2;
+              }
+              else if (data == "GetWifi")
+              {
+                step = 3;
+              }else if(data == "CountSpeedUp")
+              {
+                step = 4;
+              }
+            
           }
 
-        }break;
-        case 3:
-        {
-
-          // 写入SSID和密码到EEPROM
-          writeStringToEEPROM(ssidAddrOffset, ssid);
-          writeStringToEEPROM(passwordAddrOffset, password);
-          connectWifi(ssid.c_str(),password.c_str(),10);
-
+      }break;
+      case 1: 
+      {
+          // postConnect();
           step = 0;
+      }break;
+      case 2: 
+      {
+
+        switch(connectStep)
+        {
+          case 0:
+          {
+              connectStep = 1;
+              Serial.println(" please input ssid");
+          }break;
+          case 1:
+          {
+            if (Serial.available()) { 
+              ssid = Serial.readStringUntil('\n'); // 读取一行数据,数据要加换行符才可以
+              ssid.trim();
+              Serial.printf("you input ssid is %s and please input password\n ", ssid);
+              connectStep = 2;
+            }
+          }break;
+          case 2:
+          {
+            if (Serial.available()) {
+              password = Serial.readStringUntil('\n'); // 读取一行数据,,数据要加换行符才可以
+              password.trim();
+              Serial.printf("you input password is %s\n", password);
+              connectStep = 3;
+            }
+
+          }break;
+          case 3:
+          {
+            // 写入SSID和密码到EEPROM
+            writeStringToEEPROM(ssidAddrOffset, ssid);
+            writeStringToEEPROM(passwordAddrOffset, password);
+            connectWifi(ssid.c_str(),password.c_str(),10);
+            step = 0;
+          }break;
         }
-      }
-    }break;
-    case 3:
-    {
-      step = 0;
-      GetWifi();
-    }break;
+
+      }break;
+      case 3:
+      {
+        step = 0;
+        GetWifi();
+      }break;
+
+      case 4:
+      {
+          if (Serial.available()) {
+              
+              char SendspeedData[4][20]={"","","",""};
+              String speedData = Serial.readStringUntil('\n'); // 读取一行数据,,数据要加换行符才可以
+              speedData.trim();
+
+
+
+            for(int i=0,j=0,k=0; i<speedData.length();i++)
+            {
+                if(isdigit(speedData[i]))
+                {
+                  SendspeedData[j][k++] = speedData[i];
+                  SendspeedData[j][k] = '\0';
+                }else
+                {
+                  k=0;
+                  j++;
+                }
+            }
+
+
+            String data =  generateDataString(atoi(SendspeedData[0]),
+            atoi(SendspeedData[1]),atoi(SendspeedData[2]),atoi(SendspeedData[3]));
+
+            postConnect(1, data);
+            step = 0;
+
+          }
+
+      }break;
 
     }
 
+}
+
+String generateDataString(int count1, int count2, int count3, int count4) {
+  String data = "{\"count1\": " + String(count1) + ", \"count2\": " + String(count2) + ", \"count3\": " + String(count3) + ", \"count4\": " + String(count4) + "}";
+  return data;
 }
 
 
@@ -163,7 +205,6 @@ void GetWifi()
     }
    
 }
-
 
 
 void connectWifi( const char * ssid, const char *password,int times){
@@ -187,8 +228,7 @@ void connectWifi( const char * ssid, const char *password,int times){
 }
 
 
-
-void postConnect()
+void postConnect(char requestType,String data)
 {
 
     if (WiFi.status() == WL_CONNECTED) {
@@ -199,13 +239,14 @@ void postConnect()
       client.setInsecure();
 
       // 设置请求的URL
-      http.begin(client, "https://fc-mp-f86e4895-a6ec-4a1c-a14f-141f3d09c694.next.bspapp.com/demoArt/get");
+      http.begin(client, "https://fc-mp-f86e4895-a6ec-4a1c-a14f-141f3d09c694.next.bspapp.com/demoArt/edit");
 
       // 设置Content-Type头
       http.addHeader("Content-Type", "application/json");
 
       // 设置请求体数据
-      String requestBody = R"({"num": 2, "page": 1})";
+      String requestBody = data;
+      // String requestBody = R"({"num": 2, "page": 1})";
 
       // 发送POST请求，并接收响应
       int httpResponseCode = http.POST(requestBody);
